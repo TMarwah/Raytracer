284db78114bb3c5035b6c1db1b138fdef4b112f0
Tanmay Marwah <marwah.tanmay@gmail.com>
2023-09-28 13:45:05 -0700

    sphere complete, lighting complete, first shadows complete

--- Raytracer/src/Renderer.cpp
+++ Raytracer/src/Renderer.cpp
@@ -1,6 +1,20 @@
 #include "Renderer.h"
 #include "Walnut/Random.h"
 
+
+namespace Utils {
+	static uint32_t ConvertToRGBA(const glm::vec4& color)
+	{
+		uint8_t r = (color.r * 255.0f);
+		uint8_t g = (color.g * 255.0f);
+		uint8_t b = (color.b * 255.0f);
+		uint8_t a = (color.a * 255.0f);
+
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
+	}
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage)
@@ -15,7 +29,7 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
 
 	}
-	
+
 	delete[] m_ImageData;
 	m_ImageData = new uint32_t[width * height];
 
@@ -23,25 +37,27 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 
 void Renderer::Render()
 {
-	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++) 
+	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
 			//coord is a 0-1 float based on resolution and image dimensions
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
-			
+
 			//remap 0-1 into -1 - 1
 			coord = coord * 2.0f - 1.0f;
 
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
-		}	
-	}	
+		glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
+		}
+	}
 	m_FinalImage->SetData(m_ImageData);
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
-	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
 	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
 	float radius = 0.5;
 	// rayDirection = glm::normalize(rayDirection);
@@ -52,7 +68,7 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 	// b = ray direction
 	// r = radius of sphere
 	// t = hit distance
-	
+
 	//quadratic coefficients
 
 	//dot product of rayDirection with itself
@@ -65,11 +81,29 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 	// >0 = 2 solutions
 	// =0 = 1 solution
 	// <0 = 0 solutions
-	float discriminant = b * b - 4.0f * a * c;
 
-	if (discriminant >= 0.0f)
-		return 0xffff00ff;
-	return 0xff000000;
+	float discriminant = b * b - 4.0f * a * c;
+	if (discriminant < 0.0f)
+		return glm::vec4(0, 0, 0, 1);
+
+	// (-b +- sqrt(discriminant)) / 2a
+	float t0 = (- b + glm::sqrt(discriminant)) / (2.0f * a);
+	float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+
+	//determine both hit points, entry and exit
+	glm::vec3 hitPoint = rayOrigin + rayDirection * closestT;
+	glm::vec3 normal = glm::normalize(hitPoint);
+
+	glm::vec3 lightDir = glm::normalize(glm::vec3( - 1.0, -1.0, -1.0));
+
+
+	float d = glm::max(glm::dot(normal, -lightDir), 0.0f); // same as cosine of the angle between them between -1 to 1 (light source intensity)
+
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor *= d;
+	return glm::vec4(sphereColor, 1.0f);
+
+
 	//alpha stays 1, set green channel in 8 bit offset, set red channel at end (ABGR)
 	//return 0xff000000 | (g << 8) | r;
 }
