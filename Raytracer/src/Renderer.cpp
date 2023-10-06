bf1dfc6881c4b5eb6c9b6decc1a0b2aecf09521e
Tanmay Marwah <marwah.tanmay@gmail.com>
2023-10-05 20:31:51 -0700

    scene class abstracted multisphere now rendered

--- Raytracer/src/Renderer.cpp
+++ Raytracer/src/Renderer.cpp
@@ -35,7 +35,7 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 
 }
 
-void Renderer::Render(const Camera& camera)
+void Renderer::Render(const Scene& scene, const Camera& camera)
 {
 
 	Ray ray;
@@ -52,7 +52,7 @@ void Renderer::Render(const Camera& camera)
 			//remap 0-1 into -1 - 1
 			coord = coord * 2.0f - 1.0f;
 			ray.Direction = camera.GetRayDirections()[x + y * m_FinalImage->GetWidth()];
-			glm::vec4 color = TraceRay(ray);
+			glm::vec4 color = TraceRay(scene, ray);
 			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utils::ConvertToRGBA(color);
 		}
@@ -61,11 +61,8 @@ void Renderer::Render(const Camera& camera)
 	m_FinalImage->SetData(m_ImageData);
 }
 
-glm::vec4 Renderer::TraceRay(const Ray& ray)
+glm::vec4 Renderer::TraceRay(const Scene& scene, const Ray& ray)
 {
-	float radius = 0.5;
-	// rayDirection = glm::normalize(rayDirection);
-
 	// t is distance along the ray
 	// (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
 	// a = ray origin
@@ -75,35 +72,58 @@ glm::vec4 Renderer::TraceRay(const Ray& ray)
 
 	//quadratic coefficients
 
-	//dot product of rayDirection with itself
-	float a = glm::dot(ray.Direction, ray.Direction);
-	float b = 2.0f * glm::dot(ray.Origin, ray.Direction);
-	float c = glm::dot(ray.Origin, ray.Origin) - radius * radius;
-
-	//quadratic formula discriminant (valid solution means hit occured)
-	// b^2 - 4ac
-	// >0 = 2 solutions
-	// =0 = 1 solution
-	// <0 = 0 solutions
-
-	float discriminant = b * b - 4.0f * a * c;
-	if (discriminant < 0.0f)
+	if (scene.Spheres.size() == 0) {
 		return glm::vec4(0, 0, 0, 1);
+	}
 
-	// (-b +- sqrt(discriminant)) / 2a
-	float t0 = (- b + glm::sqrt(discriminant)) / (2.0f * a);
-	float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+	const Sphere* closestSphere = nullptr;
+	float hitDistance = FLT_MAX;
 
+	for (const Sphere& sphere : scene.Spheres) 
+	{
+		glm::vec3 origin = ray.Origin - sphere.Position;
+
+		//dot product of rayDirection with itself
+		float a = glm::dot(ray.Direction, ray.Direction);
+		float b = 2.0f * glm::dot(origin, ray.Direction);
+		float c = glm::dot(origin, origin) - sphere.Radius * sphere.Radius;
+
+		//quadratic formula discriminant (valid solution means hit occured)
+		// b^2 - 4ac
+		// >0 = 2 solutions
+		// =0 = 1 solution
+		// <0 = 0 solutions
+
+		float discriminant = b * b - 4.0f * a * c;
+		if (discriminant < 0.0f)
+			continue;
+
+		// (-b +- sqrt(discriminant)) / 2a hit calc check if closest sphere
+		// float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a);
+		
+		float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+		if (closestT < hitDistance) {
+			hitDistance = closestT;
+			closestSphere = &sphere;
+		}
+		
+
+	}
+
+	if (closestSphere == nullptr)
+		return glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	glm::vec3 origin = ray.Origin - closestSphere->Position;
 	//determine both hit points, entry and exit
-	glm::vec3 hitPoint = ray.Origin + ray.Direction * closestT;
+	glm::vec3 hitPoint = origin + ray.Direction * hitDistance;
 	glm::vec3 normal = glm::normalize(hitPoint);
 
-	glm::vec3 lightDir = glm::normalize(glm::vec3( - 1.0, -1.0, -1.0));
+	glm::vec3 lightDir = glm::normalize(glm::vec3( -1.0, -1.0, -1.0));
 
 
 	float d = glm::max(glm::dot(normal, -lightDir), 0.0f); // same as cosine of the angle between them between -1 to 1 (light source intensity)
 
-	glm::vec3 sphereColor(1, 0, 1);
+	glm::vec3 sphereColor = closestSphere->Albedo;
 	sphereColor *= d;
 	return glm::vec4(sphereColor, 1.0f);
 
